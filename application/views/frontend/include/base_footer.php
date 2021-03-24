baa1413ae4dc399996914a2931a4c1158e9bd1c7
Bahyu Sanciko <cbahyu@gmail.com>
2021-03-25 02:54:21 +0700

    fix code and add bank

--- application/views/frontend/include/base_footer.php
+++ application/views/frontend/include/base_footer.php
@@ -45,7 +45,7 @@
 					</div>
 					<div class="footer-bottom d-flex justify-content-between align-items-center flex-wrap">
 						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            <p class="footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+            <p class="footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <i class="fa fa-heart-o" aria-hidden="true"></i> </p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						<div class="footer-social d-flex align-items-center">
 							<a href="#"><i class="fa fa-facebook"></i></a>
