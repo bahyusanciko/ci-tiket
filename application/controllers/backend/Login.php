f3f235e9ef8ea478558808c3a529f0193a3d152a
Bahyu Sanciko <cbahyu@gmail.com>
2021-09-01 08:37:09 +0700

    fix code login backend

--- application/controllers/backend/Login.php
+++ application/controllers/backend/Login.php
@@ -44,34 +44,35 @@ class Login extends CI_Controller {
 		$data['ipaddres'] = $this->getUserIP();
 		$this->load->view('backend/login',$data);
 	}
+    
 	public function logout(){
 		$this->session->sess_destroy();
 		redirect(base_url('backend/login'));
 	}
+
 	public function cekuser(){
     $username = strtolower($this->input->post('username'));
-    $ambil = $this->db->query('select * from tbl_admin where username_admin = "'.$username.'"')->row_array();
+    $getUser = $this->db->query('select * from tbl_admin where username_admin = "'.$username.'"')->row();
     $password = $this->input->post('password');
 
-    if (password_verify($password,$ambil['password_admin'])) {
-    	$this->db->where('username_admin',$username);
-        $query = $this->db->get('tbl_admin');
-            foreach ($query->result() as $row) {
-                $sess = array(
-                	'kd_admin' => $row->kd_admin,
-                    'username_admin' => $row->username_admin,
-                    'password_admin' => $row->password_admin,
-                    'nama_admin'     => $row->nama_admin,
-                    'img_admin'	=> $row->img_admin,
-                    'email_admin'   => $row->email_admin,
-                    'telpon_admin'   => $row->telpon_admin,
-                    'alamat_admin'	=> $row->alamat_admin,
-                    'level'	=> $row->level_admin
-                );
-                // die(print_r($sess));
-                $this->session->set_userdata($sess);
-                redirect('backend/home');
-            }
+    if (password_verify($password,$getUser->password_admin)) {
+    	// $this->db->where('username_admin',$username);
+        // $query = $this->db->get('tbl_admin');
+        $sess = array(
+            'kd_admin' => $getUser->kd_admin,
+            'username_admin' => $getUser->username_admin,
+            'password_admin' => $getUser->password_admin,
+            'nama_admin'     => $getUser->nama_admin,
+            'img_admin'	=> $getUser->img_admin,
+            'email_admin'   => $getUser->email_admin,
+            'telpon_admin'   => $getUser->telpon_admin,
+            'alamat_admin'	=> $getUser->alamat_admin,
+            'level'	=> $getUser->level_admin
+        );
+        // die(print_r($sess));
+        $this->session->set_userdata($sess);
+        redirect('backend/home');
+        // }
     }else{
     	$this->session->set_flashdata('message', 'swal("Gagal", "Email/Password Salah", "error");');
     	redirect('backend/login');
